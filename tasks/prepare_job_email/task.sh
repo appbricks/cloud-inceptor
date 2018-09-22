0501a7f50cbda1edc25109fbe495e0298e5318a8
Mevan Samaratunga <mevansam@gmail.com>
2018-09-22 15:19:58 +0400

    improved tract

--- tasks/prepare_job_email/task.sh
+++ tasks/prepare_job_email/task.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-set -xeuo pipefail
+set -euo pipefail
 
 wget -O mc "http://dl.minio.io/client/mc/release/linux-amd64/mc" && chmod +x mc
 ./mc config host add auto $AUTOS3_URL $AUTOS3_ACCESS_KEY $AUTOS3_SECRET_KEY
 
 wget -O fly "$CONCOURSE_URL/api/v1/cli?arch=amd64&platform=linux" && chmod +x fly
 ./fly -t default login -c $CONCOURSE_URL -u ''$CONCOURSE_USER'' -p ''$CONCOURSE_PASSWORD''
+
+set -x
 
 cat <<EOF > emails/headers
 MIME-version: 1.0
@@ -19,7 +21,10 @@ for email in $emails; do
   ./mc cp $email job_info
   source job_info
 
+  set +x
   job_output=$(./fly -t default watch -j $BUILD_PIPELINE_NAME/$BUILD_JOB_NAME -b $BUILD_NAME)
+  set -x
+  
   echo -e "$job_output" \
     | automation/lib/inceptor/tasks/prepare_job_email/ansi2html.sh --bg=dark --palette=tango \
     > emails/email_body_$i.html
