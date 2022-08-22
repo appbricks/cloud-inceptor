2fbb541998f1e83e8106c251f304df80055aebf0
Mevan Samaratunga <mevansam@gmail.com>
2022-08-22 11:09:21 -0400

    install docker on jumpbox

--- modules/bootstrap/aws/jumpbox.tf
+++ modules/bootstrap/aws/jumpbox.tf
@@ -51,7 +51,24 @@ write_files:
   permissions: '0744'
 
 runcmd: 
-- sudo /root/mount-volume.sh
+
+# Install Docker
+- |
+  apt-get update
+  apt-get install -y ca-certificates curl gnupg lsb-release
+  
+  mkdir -p /etc/apt/keyrings \
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+  apt-get update
+  apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Mount data volume
+- /root/mount-volume.sh
+
 USERDATA
 }
 
