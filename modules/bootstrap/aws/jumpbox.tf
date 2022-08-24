9bc7815ab242df3019b1775ebcb851d939968337
Mevan Samaratunga <mevansam@gmail.com>
2022-08-24 09:54:30 -0400

    handle install on debian or ubuntu distros

--- modules/bootstrap/aws/jumpbox.tf
+++ modules/bootstrap/aws/jumpbox.tf
@@ -59,12 +59,13 @@ runcmd:
 # Install Docker
 - |
   apt-get update
-  apt-get install -y ca-certificates curl gnupg lsb-release
+  apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
   
   mkdir -p /etc/apt/keyrings
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
   echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+    https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
   
   apt-get update
